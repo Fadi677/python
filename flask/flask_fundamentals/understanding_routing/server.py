341a5eb180430e67502c12de5c8dd3a5dd3d9b7a
Fadi Abuaita <fadimabuaita@gmail.com>
2022-03-22 17:36:42 +0200

    understanding routing assignment-flask

--- flask/flask_fundamentals/understanding_routing/server.py
+++ flask/flask_fundamentals/understanding_routing/server.py
@@ -1,21 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 @app.route('/dojo')         
 def dojo():
     return 'Dojo!'
+
 @app.route('/say/<name>')         
 def saysth(name):
     return "Hi " + name
-@app.route('/repeat/<repeat>/<textme>')         
+
+@app.route('/repeat/<int:repeat>/<textme>')         
 def repeatme(repeat,textme):
-    # if isinstance(repeat, int)==True:
-    return textme*int(repeat)
-    # else:
-        # return "please enter an integer as a repeat value"
+    if isinstance(repeat, int):
+        return textme*int(repeat)
+    else:
+        return "please enter an integer as a repeat value"
 
 
-if __name__=="__main__":  
+if __name__=="__main__":
     app.run(debug=True)
